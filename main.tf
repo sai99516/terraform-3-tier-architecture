3b09d69ed1cbc404eb0b343c638b1e1fc2b14670
sai99516 <143603007+sai99516@users.noreply.github.com>
2025-01-22 10:49:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,8 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.69.0"
-    }
-  }
-}
-
-terraform {
   backend "s3" {
-    bucket = "terrastatebyucket0078970778"
+    bucket = "saibucket"  # Change this to your new bucket name
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-1"  # Change region if necessary
   }
 }
 
@@ -50,8 +41,6 @@ resource "aws_subnet" "web-subnet-2" {
     Name = "swiggy-Web-subnet-1b"
   }
 }
-
-
 
 # Create Private Subnet for app-server-1
 resource "aws_subnet" "application-subnet-1" {
@@ -99,7 +88,6 @@ resource "aws_subnet" "database-subnet-2" {
   }
 }
 
-
 # Create Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.my-vpc.id
@@ -109,11 +97,9 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-
 # Create Web layber route table
 resource "aws_route_table" "web-rt" {
   vpc_id = aws_vpc.my-vpc.id
-
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -124,7 +110,6 @@ resource "aws_route_table" "web-rt" {
     Name = "WebRT"
   }
 }
-
 
 # Create Web Subnet association with Web route table
 resource "aws_route_table_association" "a" {
@@ -137,240 +122,12 @@ resource "aws_route_table_association" "b" {
   route_table_id = aws_route_table.web-rt.id
 }
 
-#Create WEB-SERVER-1 Instance
-resource "aws_instance" "webserver1" {
-  ami                    = "ami-0d5eff06f840b45e9"
-  instance_type          = "t2.micro"
-  availability_zone      = "us-east-1a"
-  key_name               = "dockerr"
-  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
-  subnet_id              = aws_subnet.web-subnet-1.id
-  user_data              = file("apache.sh")
+# Add other resources as needed (Instances, Security Groups, ALB, etc.)
 
-  tags = {
-    Name = "SWIGGY-Web-Server-1"
-  }
-}
-
-#Create WEB-SERVER-2 Instance
-resource "aws_instance" "webserver2" {
-  ami                    = "ami-0d5eff06f840b45e9"
-  instance_type          = "t2.micro"
-  availability_zone      = "us-east-1b"
-  key_name               = "dockerr"
-  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
-  subnet_id              = aws_subnet.web-subnet-2.id
-  user_data              = file("apache.sh")
-
-  tags = {
-    Name = "SWIGGY-Web-Server-2"
-  }
-}
-
-#Create APP SERVER-1 EC2 Instance
-resource "aws_instance" "appserver1" {
-  ami                    = "ami-0d5eff06f840b45e9"
-  instance_type          = "t2.micro"
-  availability_zone      = "us-east-1a"
-  key_name               = "dockerr"
-  vpc_security_group_ids = [aws_security_group.appserver-sg.id]
-  subnet_id              = aws_subnet.application-subnet-1.id
-  tags = {
-    Name = "SWIGGY-app-Server-1"
-  }
-}
-
-#Create APP SERVER-2 EC2 Instance
-resource "aws_instance" "appserver2" {
-  ami                    = "ami-0d5eff06f840b45e9"
-  instance_type          = "t2.micro"
-  availability_zone      = "us-east-1b"
-  key_name               = "dockerr"
-  vpc_security_group_ids = [aws_security_group.appserver-sg.id]
-  subnet_id              = aws_subnet.application-subnet-2.id
-
-  tags = {
-    Name = "app Server-2"
-  }
-}
-
-resource "aws_db_instance" "default" {
-  allocated_storage      = 10
-  db_name                = "mydb"
-  engine                 = "mysql"
-  engine_version         = "8.0"
-  instance_class         = "db.t3.micro"
-  username               = "admin"
-  password               = "Raham#444555"
-  parameter_group_name   = "default.mysql8.0"
-  skip_final_snapshot    = true
-  vpc_security_group_ids = [aws_security_group.database-sg.id]
-  db_subnet_group_name   = aws_db_subnet_group.default.id
-}
-
-resource "aws_db_subnet_group" "default" {
-  name       = "main"
-  subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-2.id]
-
-  tags = {
-    Name = "My DB subnet group"
-  }
-}
-
-# Create Web Security Group
-resource "aws_security_group" "webserver-sg" {
-  name        = "webserver-sg"
-  description = "Allow HTTP inbound traffic"
-  vpc_id      = aws_vpc.my-vpc.id
-
-  ingress {
-    description = "HTTP from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "Web-SG"
-  }
-}
-
-# Create Application Security Group
-resource "aws_security_group" "appserver-sg" {
-  name        = "appserver-SG"
-  description = "Allow inbound traffic from ALB"
-  vpc_id      = aws_vpc.my-vpc.id
-
-  ingress {
-    description = "Allow traffic from web layer"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow traffic from web layer"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "appserver-SG"
-  }
-}
-
-# Create Database Security Group
-resource "aws_security_group" "database-sg" {
-  name        = "Database-SG"
-  description = "Allow inbound traffic from application layer"
-  vpc_id      = aws_vpc.my-vpc.id
-
-  ingress {
-    description = "Allow traffic from application layer"
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 32768
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "Database-SG"
-  }
-}
-
-#CREATE LOAD-BALANCER FOR SWIGGY
-
-resource "aws_lb" "external-elb" {
-  name               = "SWIGGY-LB"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.webserver-sg.id]
-  subnets            = [aws_subnet.web-subnet-1.id, aws_subnet.web-subnet-2.id]
-}
-
-resource "aws_lb_target_group" "external-elb" {
-  name     = "ALB-TG"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.my-vpc.id
-}
-
-resource "aws_lb_target_group_attachment" "external-elb1" {
-  target_group_arn = aws_lb_target_group.external-elb.arn
-  target_id        = aws_instance.webserver1.id
-  port             = 80
-
-  depends_on = [
-    aws_instance.webserver1,
-  ]
-}
-
-resource "aws_lb_target_group_attachment" "external-elb2" {
-  target_group_arn = aws_lb_target_group.external-elb.arn
-  target_id        = aws_instance.webserver2.id
-  port             = 80
-
-  depends_on = [
-    aws_instance.webserver2,
-  ]
-}
-
-resource "aws_lb_listener" "external-elb" {
-  load_balancer_arn = aws_lb.external-elb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.external-elb.arn
-  }
-}
-
-
-
-
-output "lb_dns_name" {
-  description = "The DNS name of the load balancer"
-  value       = aws_lb.external-elb.dns_name
-}
-
-
+# AWS S3 Bucket resource (replace with your actual bucket name)
 resource "aws_s3_bucket" "example" {
-  bucket = "pipleinbucket0088bdhuwtrrrrf3t5hd8e8r"
+  bucket = "saibucket"  # Updated bucket name
 }
-
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.example.id
@@ -379,25 +136,6 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
   }
 }
 
-resource "aws_iam_group_membership" "team" {
-  name = "tf-testing-group-membership"
-
-  users = [
-    aws_iam_user.user_one.name,
-    aws_iam_user.user_two.name,
-  ]
-
-  group = aws_iam_group.group.name
-}
-
-resource "aws_iam_group" "group" {
-  name = "test-group"
-}
-
-resource "aws_iam_user" "user_one" {
-  name = "test-user"
-}
-
-resource "aws_iam_user" "user_two" {
-  name = "test-user-two"
+output "bucket_name" {
+  value = aws_s3_bucket.example.bucket
 }
